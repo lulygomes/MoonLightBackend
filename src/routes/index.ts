ff2dadc1eb3fa15c9c2c4093115ac5beab757597
Luiz Silva <luiz.gsilva42@gmail.com>
2021-01-19 23:29:10 -0300

    add Cors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import cors from 'cors';
 
 import customersRouter from './customers.routes';
 import usersRouter from './users.routes';
 import sessionsRouter from './sessions.routes';
 
 const routes = Router();
+
+routes.use(cors);
 
 routes.use('/customers', customersRouter);
 routes.use('/users', usersRouter);
